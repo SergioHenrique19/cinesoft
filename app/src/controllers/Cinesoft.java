71765d373c485582027411bd94e232bb5901bcbe
João Victor T. Salgado <jvictorteo2000@hotmail.com>
2019-06-16 20:35:20 -0300

    Finalização das logicas de SELECT e INSERT.

--- app/src/controllers/Cinesoft.java
+++ app/src/controllers/Cinesoft.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.sql.ResultSet;
 import java.text.ParseException;
 
 import java.sql.Connection;
@@ -13,18 +14,32 @@ import repository.Database;
 public class Cinesoft {
 	public static void main(String[] args) throws ParseException {
 		Database db = new Database();
-		Connection c = db.open();
+		Connection conn = db.open();
+		//db.insert(conn, "filmes", "titulo, lancamento, duracao", "'Avatar', '2009-05-01', 180");
+		//ResultSet result = db.select(conn, "*", "filmes");
+		//ResultSet result = db.select(conn, "*", "filmes", "id == 1");
+		/*while(true)
+		{
+			try {
+				if (!result.next()) break;
+				System.out.println(result.getString("titulo"));
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 
-		Controlador.cadastrarFilme(0, "avatar", "25/09/2010", 120, "12");		
+		}
+		db.close(conn);*/
+
+		/*Controlador.cadastrarFilme(0, "avatar", "25/09/2010", 120, "12");
 		System.out.println(Controlador.listaFilmes.get(0));
-		
+
 		Controlador.listaFilmes.get(0).setTitulo("ava");
 		System.out.println(Controlador.listaFilmes.get(0).getTitulo());
-		
+
 		System.out.println(Controlador.buscarFilme("avatar"));
-		
+
 		//Teste para salvar sessao na lista
 		Controlador.cadastrarSessao(0, 19, "ava", "dub", "2d");
-		System.out.println(Controlador.listaSessoes.get(0).toString());
+		System.out.println(Controlador.listaSessoes.get(0).toString());*/
 	}
 }
--- app/src/repository/Database.java
+++ app/src/repository/Database.java
@@ -15,7 +15,7 @@ public class Database {
             ResultSet rs = stmt.executeQuery("SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%';");
             System.out.println("# Following tables has been found:");
             while (rs.next()) {
-                System.out.println("# " + rs.getString("name"));
+                System.out.println("# -" + rs.getString("name"));
             }
 
         } catch (SQLException e) {
@@ -24,6 +24,60 @@ public class Database {
         }
         return conn;
     }
+
+    public ResultSet select(Connection conn, String fields, String table)
+    {
+        try {
+            String sql = "SELECT " + fields +  " FROM " + table +";";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            return rs;
+        } catch (SQLException e) {
+            System.out.println("# Couldn't run SELECT query due to:");
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public ResultSet select(Connection conn, String fields, String table, String where)
+    {
+        try {
+            String sql = "SELECT " + fields +  " FROM " + table + " WHERE " + where;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            return rs;
+        } catch (SQLException e) {
+            System.out.println("# Couldn't run SELECT query due to:");
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public int insert(Connection conn, String table, String fields, String values)
+    {
+        int id = -1;
+        try{
+            String sql = "INSERT INTO " + table + " (" + fields + ") VALUES("+ values +");";
+            Statement stmt = conn.createStatement();
+            int affected = stmt.executeUpdate(sql);
+            System.out.println("# Rows affected by INSERT: " + affected);
+
+            Statement getId = conn.createStatement();
+            ResultSet rs = getId.executeQuery("SELECT last_insert_rowid();");
+            while (rs.next()) {
+                id = rs.getInt("last_insert_rowid()");
+                System.out.println("# Last insertion ID: " + id);
+            }
+            return id;
+        }
+        catch (SQLException e)
+        {
+            System.out.println("# Couldn't run INSERT query due to:");
+            System.out.println(e.getMessage());
+            return id;
+        }
+    }
+
     public void close(Connection conn)
     {
         try
