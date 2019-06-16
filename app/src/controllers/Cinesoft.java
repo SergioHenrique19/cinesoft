acb285686159d110a3211883ee1e08c2541e19f5
SergioHenrique19 <garciasergiodm@gmail.com>
2019-06-16 20:41:23 -0300

    Atualizado controllers

--- app/src/controllers/Cinesoft.java
+++ app/src/controllers/Cinesoft.java
@@ -1,7 +1,11 @@
+/* Trabalho Final de PLP (2019/01)
+ * Este codigo a interacao com o usuario.
+ * Desenvolvido por: Gabriel Peruzza, João Salgado, Otavio Lima e Sérgio Garcia */
+
 package controllers;
 
 import java.text.ParseException;
-
+import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,17 +18,142 @@ public class Cinesoft {
 	public static void main(String[] args) throws ParseException {
 		Database db = new Database();
 		Connection c = db.open();
+		Scanner sc = new Scanner(System.in);
+		int opcao = 0;
 
-		Controlador.cadastrarFilme(0, "avatar", "25/09/2010", 120, "12");		
-		System.out.println(Controlador.listaFilmes.get(0));
-		
-		Controlador.listaFilmes.get(0).setTitulo("ava");
-		System.out.println(Controlador.listaFilmes.get(0).getTitulo());
-		
-		System.out.println(Controlador.buscarFilme("avatar"));
-		
-		//Teste para salvar sessao na lista
-		Controlador.cadastrarSessao(0, 19, "ava", "dub", "2d");
-		System.out.println(Controlador.listaSessoes.get(0).toString());
+		while(opcao != 9) {
+			System.out.println("------- C I N E S O F T -------");
+			System.out.println(" Digite uma operação:");
+			System.out.println(" (1) Cadastrar filme");
+			System.out.println(" (2) Listar filmes");
+			System.out.println(" (3) Cadastrar sessão");
+			System.out.println(" (4) Alterar sessão");
+			System.out.println(" (5) Listar sessões");
+			System.out.println(" (6) Deletar sessão");
+			System.out.println(" (7) Vender ingresso");
+			System.out.println(" (9) Sair");
+			System.out.println(" Opcao: ");
+			opcao = sc.nextInt();
+
+			switch(opcao) {
+				case 1:
+					System.out.println("#  C A D A S T R A R  F I L M E  #");
+					System.out.println(" Id do filme: ");
+					int id = sc.nextInt();
+					System.out.println(" Titulo do filme: ");
+					String tit = sc.next();
+					System.out.println(" Data de lançamento (dd/mm/aaaa): ");
+					String datah = sc.next();
+					System.out.println(" Duração (min): ");
+					int dur = sc.nextInt();
+					System.out.println(" Classificação indicativa: ");
+					String cf = sc.next();
+
+					Controlador.cadastrarFilme(id, tit, datah, dur, cf);
+
+					break;
+
+				case 2:
+					System.out.println("#  L I S T A R  F I L M E S  #");
+
+					for(Filme filme : Controlador.listaFilmes) {
+						System.out.println(filme);
+					}
+
+					break;
+
+				case 3:
+					System.out.println("#  C A D A S T R A R  S E S S A O  #");
+					System.out.println(" Id da sessão: ");
+					int idS = sc.nextInt();
+					System.out.println(" Hora da sessao (apenas numero): ");
+					int hora = sc.nextInt();
+					System.out.println(" Filme: ");
+					String tits = sc.next();
+					System.out.println(" Idioma (dub ou leg): ");
+					String idiomah = sc.next();
+					System.out.println(" Tela (2d ou 3d): ");
+					String telahs = sc.next();
+
+					Controlador.cadastrarSessao(idS, hora, tits, idiomah, telahs);
+
+					break;
+
+				case 4:
+					System.out.println("#  A L T E R A R  S E S S A O  #");
+					System.out.println(" Id da sessao: ");
+					int idA = sc.nextInt();
+					System.out.println(" Dado a alterar");
+					System.out.println(" (1) Hora");
+					System.out.println(" (2) Filme");
+					System.out.println(" (3) Idioma");
+					System.out.println(" (4) Tela");
+					System.out.println(" Opcao: ");
+					int op = sc.nextInt();
+
+					Controlador.alterarSessao(idA, op);
+
+					break;
+
+				case 5:
+					System.out.println("#  L I S T A R  S E S S O E S  #");
+
+					for(Sessao sessao : Controlador.listaSessoes) {
+						System.out.println(sessao);
+					}
+
+					break;
+
+				case 6:
+					System.out.println("#  D E L E T A R  S E S S A O  #");
+					System.out.println(" Id da sessao: ");
+					int idD = sc.nextInt();
+
+					Controlador.removerSessao(idD);
+
+					break;
+
+				case 7:
+					System.out.println("#  V E N D E R  I N G R E S S O  #");
+					System.out.println(" Tipo de ingresso - Inteira (1) ou Meio (2): ");
+					int opI = sc.nextInt();
+
+					int idI, idIS, pol = 0;
+					String dataI = "";
+
+					System.out.println(" Id do ingresso: ");
+					idI = sc.nextInt();
+					System.out.println(" Data: ");
+					dataI = sc.next();
+					System.out.println(" Id da sessao: ");
+					idIS = sc.nextInt();
+					System.out.println(" Poltrona: ");
+					pol = sc.nextInt();
+
+					if(opI == 1) {
+						Controlador.cadastrarIngrInt(idI, dataI, idIS, pol);
+					} else if(opI == 2) {
+						System.out.println(" Carteirinha (id_jovem, estudante, fidelidade): ");
+						String card = sc.next();
+
+						Controlador.cadastrarIngrMeio(idI, dataI, idIS, pol, card);
+					} else {
+						System.out.println("Opcao invalida...");
+					}
+
+					break;
+
+				case 9:
+					System.out.println("Volte sempre...");
+
+					break;
+
+				default:
+					System.out.println("ERROR: OPCAO INVALIDA!!!");
+					break;
+			}
+		}
+
+		sc.close();
 	}
 }
--- app/src/controllers/Controlador.java
+++ app/src/controllers/Controlador.java
@@ -1,3 +1,7 @@
+/* Trabalho Final de PLP (2019/01)
+ * Este codigo contem todas as operacoes possiveis das classes em models.
+ * Desenvolvido por: Gabriel Peruzza, João Salgado, Otavio Lima e Sérgio Garcia */
+
 package controllers;
 
 import java.text.ParseException;
@@ -13,175 +17,217 @@ public class Controlador {
 	// Listas de filmes e de sessoes
 	protected static List<Filme> listaFilmes = new ArrayList<Filme>();
 	protected static List<Sessao> listaSessoes = new ArrayList<Sessao>();
-	
-	// Metodo para cadastrar um filme na lista e no arquivo
+	protected static List<IngressoInteiro> listaIngInts = new ArrayList<IngressoInteiro>();
+	protected static List<IngressoMeio> listaIngMeios = new ArrayList<IngressoMeio>();
+
+	// Metodo para cadastrar um filme na lista
 	public static void cadastrarFilme(int idFilme, String titulo, String datah, int duracao, String classific) throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		ClassIndic classIndic = null;
 		Date dataLancamento = sdf.parse(datah);
-		
+
 		switch(classific) {
 			case "l":
 				classIndic = ClassIndic.LIVRE;
 				break;
-			
+
 			case "10":
 				classIndic = ClassIndic.DEZ;
 				break;
-			
+
 			case "12":
 				classIndic = ClassIndic.DOZE;
 				break;
-			
+
 			case "14":
 				classIndic = ClassIndic.CATORZE;
 				break;
-			
+
 			case "16":
 				classIndic = ClassIndic.DEZESSEIS;
 				break;
-			
+
 			case "18":
 				classIndic = ClassIndic.DEZOITO;
 				break;
-			
+
 			default:
 				classIndic = null;
 				break;
 		}
-		
+
 		Filme f = new Filme(idFilme, titulo, dataLancamento, duracao, classIndic);
 		listaFilmes.add(f);
 	}
-	
+
 	// Funcao que busca filme pelo titulo na lista do mesmo
 	public static Filme buscarFilme(String busca) {
 		for(Filme filme : listaFilmes) {
 			if(filme.getTitulo().equals(busca)) {
 				return filme;
 			}
-		}		
+		}
 		return null;
 	}
-	
-	// Metodo para cadastrar uma sessao na lista e no arquivo
+
+	// Metodo para cadastrar uma sessao na lista
 	public static void cadastrarSessao(int idSessao, int hora, String filmeh, String idiomah, String telah) {
 		Filme filme = buscarFilme(filmeh);
 		Idioma idioma = null;
 		Tela tela = null;
-		
+
 		if(filme.equals(null)) {
 			System.out.println("Filme nao encontrado...");
 		}
-		
+
 		switch(idiomah) {
 			case "dub":
 				idioma = Idioma.DUBLADO;
 				break;
-			
+
 			case "leg":
 				idioma = Idioma.LEGENDADO;
 				break;
-			
+
 			default:
 				tela = null;
 				break;
 		}
-		
+
 		switch(telah) {
 			case "2d":
 				tela = Tela.DOIS_D;
 				break;
-			
+
 			case "3d":
 				tela = Tela.TRES_D;
 				break;
-			
+
 			default:
 				tela = null;
 				break;
 		}
-		
+
 		Sessao s = new Sessao(idSessao, hora, filme, idioma, tela);
 		listaSessoes.add(s);
 	}
-	
+
 	// Funcao para buscar sessao pelo id na lista do mesmo
 	public static Sessao buscarSessao(int busca) {
 		for(Sessao sessao : listaSessoes) {
 			if(sessao.getIdSessao() == busca) {
 				return sessao;
 			}
-		}		
+		}
 		return null;
 	}
-	
+
 	// Metodo para alterar sessao pelo id
 	public static void alterarSessao(int id, int opcao) {
 		Scanner sc = new Scanner(System.in);
 		int ind = -1;
-		
+
 		for(Sessao sessao : listaSessoes) {
 			if(sessao.getIdSessao() == id) {
 				ind = listaSessoes.indexOf(sessao);
 			}
 		}
-		
+
 		switch(opcao) {
 			case 1:
 				System.out.println("Digite a nova hora: ");
 				int nHora = sc.nextInt();
 				listaSessoes.get(ind).setHora(nHora);
 				break;
-			
+
 			case 2:
 				System.out.println("Digite o titulo do filme: ");
 				String ntitulo = sc.next();
-				
+
 				Filme nFilme = buscarFilme(ntitulo);
 				listaSessoes.get(ind).setFilme(nFilme);
 				break;
-			
+
 			case 3:
 				System.out.println("Digite o idioma: ");
 				String nIdiomah = sc.next();
-				
+
 				Idioma nIdioma = null;
-				
+
 				if(nIdiomah.equals("dub")) {
 					nIdioma = Idioma.DUBLADO;
 				} else {
 					nIdioma = Idioma.LEGENDADO;
 				}
-				
+
 				listaSessoes.get(ind).setIdioma(nIdioma);
-			
+
 			case 4:
 				System.out.println("Digite a tela: ");
 				String nTelah = sc.next();
-				
+
 				Tela nTela = null;
-				
+
 				if(nTelah.equals("2d")) {
 					nTela = Tela.DOIS_D;
 				} else {
 					nTela = Tela.TRES_D;
 				}
-				
+
 				listaSessoes.get(ind).setTela(nTela);
 		}
+		sc.close();
 	}
-	
+
 	// Metodo para remover sessao pelo id
 	public static void removerSessao(int id) {
 		int ind = -1;
-		
+
 		for(Sessao sessao : listaSessoes) {
 			if(sessao.getIdSessao() == id) {
 				ind = listaSessoes.indexOf(sessao);
 			}
 		}
-		
+
 		listaSessoes.remove(ind);
+	}
+
+	// Metodo para cadastrar ingresso inteiro
+	public static void cadastrarIngrInt(int idIngresso, String datah, int idSes, int poltrona) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date dataVenda = sdf.parse(datah);
+		Sessao sessao = buscarSessao(idSes);
+
+		IngressoInteiro ingInt = new IngressoInteiro(idIngresso, dataVenda, sessao, poltrona);
+		listaIngInts.add(ingInt);
+	}
+
+	// Metodo para cadastrar ingresso meio
+	public static void cadastrarIngrMeio(int idIngresso, String datah, int idSes, int poltrona, String carteirah) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date dataVenda = sdf.parse(datah);
+		Sessao sessao = buscarSessao(idSes);
+		Carteirinha carteirinha = null;
+
+		switch(carteirah) {
+			case "id_jovem":
+				carteirinha = Carteirinha.ID_JOVEM;
+				break;
+
+			case "estudante":
+				carteirinha = Carteirinha.ESTUDANTE;
+				break;
+
+			case "fidelidade":
+				carteirinha = Carteirinha.FIDELIDADE;
+				break;
+
+			default:
+				carteirinha = null;
+				break;
+		}
+
+		IngressoMeio ingMeio = new IngressoMeio(idIngresso, dataVenda, sessao, poltrona, carteirinha);
+		listaIngMeios.add(ingMeio);
 	}
 }
